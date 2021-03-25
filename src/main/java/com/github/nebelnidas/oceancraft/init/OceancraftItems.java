bec8ea552a9a3ec4734c1cef7f5261e0cf2a9c58
Cicero Interactive <burnerjulian@gmail.com>
2021-03-25 09:22:39 +0100

    Add coral items

--- src/main/java/com/github/nebelnidas/oceancraft/init/OceancraftItems.java
+++ src/main/java/com/github/nebelnidas/oceancraft/init/OceancraftItems.java
@@ -28,6 +28,12 @@ public class OceancraftItems {
 	public static BlockItem THICK_SEAWEED_PLANT_BLOCK;
 	public static BlockItem SEAWEED_BLOCK;
 
+	public static BlockItem SEAROD_CORAL;
+	public static BlockItem SEAWHIP_CORAL;
+	public static BlockItem SHEET_CORAL;
+	public static BlockItem STAR_CORAL;
+	public static BlockItem BRAIN_CORAL;
+
 	public static Item CORAL_DUST;
 	public static Item CORAL_INGOT;
 
@@ -51,10 +57,20 @@ public class OceancraftItems {
 
 
 	public static void init() {
+		// ------------------------------------------------------------
+		// Seaweed
 		SEAWEED_PLANT_BLOCK = new BlockItem(OceancraftBlocks.SEAWEED_PLANT_BLOCK, DEFAULT_ITEM_SETTINGS);
 		THICK_SEAWEED_PLANT_BLOCK = new BlockItem(OceancraftBlocks.THICK_SEAWEED_PLANT_BLOCK, DEFAULT_ITEM_SETTINGS);
 		SEAWEED_BLOCK = new BlockItem(OceancraftBlocks.SEAWEED_BLOCK, DEFAULT_ITEM_SETTINGS);
 
+		// Corals
+		SEAROD_CORAL = new BlockItem(OceancraftBlocks.SEAROD_CORAL, DEFAULT_ITEM_SETTINGS);
+		SEAWHIP_CORAL = new BlockItem(OceancraftBlocks.SEAWHIP_CORAL, DEFAULT_ITEM_SETTINGS);
+		SHEET_CORAL = new BlockItem(OceancraftBlocks.SHEET_CORAL, DEFAULT_ITEM_SETTINGS);
+		STAR_CORAL = new BlockItem(OceancraftBlocks.STAR_CORAL, DEFAULT_ITEM_SETTINGS);
+		BRAIN_CORAL = new BlockItem(OceancraftBlocks.BRAIN_CORAL, DEFAULT_ITEM_SETTINGS);
+
+		// Coral tools
 		CORAL_DUST = new Item(DEFAULT_ITEM_SETTINGS);
 		CORAL_INGOT = new Item(DEFAULT_ITEM_SETTINGS);
 		CORAL_SWORD = new CoralSwordItem(CORAL_TOOL_MATERIAL, DEFAULT_ITEM_SETTINGS);
@@ -63,13 +79,16 @@ public class OceancraftItems {
 		CORAL_PICKAXE = new CoralPickaxeItem(new CoralToolMaterial(), DEFAULT_ITEM_SETTINGS);
 		CORAL_HOE = new CoralHoeItem(CORAL_TOOL_MATERIAL, DEFAULT_ITEM_SETTINGS);
 
+		// Coral armor
 		CORAL_HELMET = new OceancraftArmorItem(CORAL_ARMOR_MATERIAL, EquipmentSlot.HEAD, DEFAULT_ITEM_SETTINGS);
 		CORAL_CHESTPLATE = new OceancraftArmorItem(CORAL_ARMOR_MATERIAL, EquipmentSlot.CHEST, DEFAULT_ITEM_SETTINGS);
 		CORAL_LEGGINGS = new OceancraftArmorItem(CORAL_ARMOR_MATERIAL, EquipmentSlot.LEGS, DEFAULT_ITEM_SETTINGS);
 		CORAL_BOOTS = new OceancraftArmorItem(CORAL_ARMOR_MATERIAL, EquipmentSlot.FEET, DEFAULT_ITEM_SETTINGS);
 
+		// Turtle armor
 		TURTLE_SHELL = new OceancraftArmorItem(OCEANCRAFT_TURTLE_ARMOR_MATERIAL, EquipmentSlot.CHEST, DEFAULT_ITEM_SETTINGS);
 
+		// Pearls
 		WHITE_PEARL = new Item(DEFAULT_ITEM_SETTINGS);
 		BLACK_PEARL = new Item(DEFAULT_ITEM_SETTINGS);
 		WATER_PEARL = new WaterPearlItem(DEFAULT_ITEM_SETTINGS);
@@ -77,10 +96,20 @@ public class OceancraftItems {
 
 
 
+		// ------------------------------------------------------------
+		// Seaweed
 		SEAWEED_PLANT_BLOCK = registerItem(SEAWEED_PLANT_BLOCK, "seaweed_plant_block");
 		THICK_SEAWEED_PLANT_BLOCK = registerItem(THICK_SEAWEED_PLANT_BLOCK, "thick_seaweed_plant_block");
 		SEAWEED_BLOCK = registerItem(SEAWEED_BLOCK, "seaweed_block");
 
+		// Corals
+		SEAROD_CORAL = registerItem(SEAROD_CORAL, "searod_coral");
+		SEAWHIP_CORAL = registerItem(SEAWHIP_CORAL, "seawhip_coral");
+		SHEET_CORAL = registerItem(SHEET_CORAL, "sheet_coral");
+		STAR_CORAL = registerItem(STAR_CORAL, "star_coral");
+		BRAIN_CORAL = registerItem(BRAIN_CORAL, "brain_coral");
+
+		// Coral tools
 		CORAL_DUST = registerItem(CORAL_DUST,"coral_dust");
 		CORAL_INGOT = registerItem(CORAL_INGOT, "coral_ingot");
 		CORAL_SWORD = registerItem(CORAL_SWORD, "coral_sword");
@@ -89,13 +118,16 @@ public class OceancraftItems {
 		CORAL_PICKAXE = registerItem(CORAL_PICKAXE, "coral_pickaxe");
 		CORAL_HOE = registerItem(CORAL_HOE, "coral_hoe");
 
+		// Coral armor
 		CORAL_HELMET = registerItem(CORAL_HELMET, "coral_helmet");
 		CORAL_CHESTPLATE = registerItem(CORAL_CHESTPLATE, "coral_chestplate");
 		CORAL_LEGGINGS = registerItem(CORAL_LEGGINGS, "coral_leggings");
 		CORAL_BOOTS = registerItem(CORAL_BOOTS, "coral_boots");
 
+		// Turtle armor
 		TURTLE_SHELL = registerItem(TURTLE_SHELL, "turtle_shell");
 
+		// Pearls
 		WHITE_PEARL = registerItem(WHITE_PEARL, "white_pearl");
 		BLACK_PEARL = registerItem(BLACK_PEARL, "black_pearl");
 		WATER_PEARL = registerItem(WATER_PEARL, "water_pearl");
